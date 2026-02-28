1a0807c48bedda519253858f5c4e5c976bb15e02
Esteban Urrutia <esteuwu@proton.me>
2026-02-28 12:15:11 -0300

    sm8550-common: Remove blob fixups no longer needed
    Change-Id: I731c5a019a9a73c27ba4377f01e0ef2a902ea824

--- extract-files.py
+++ extract-files.py
@@ -72,8 +72,6 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('.*media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio).*\n', ''),
     ('vendor/bin/hw/motorola.hardware.sensorext-service', 'vendor/lib64/libdpps.so', 'vendor/lib64/libsnapdragoncolor-manager.so'): blob_fixup()
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
-    'vendor/lib64/libqcodec2_core.so': blob_fixup()
-        .add_needed('libcodec2_shim.so'),
     'vendor/lib64/sensors.moto.so': blob_fixup()
         .add_needed('libbase_shim.so'),
     'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
