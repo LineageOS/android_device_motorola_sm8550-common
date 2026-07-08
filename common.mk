de09875f98298041036864c34d95dc6e9eebfc3c
Nolen Johnson <johnsonnolen@gmail.com>
2026-07-08 13:32:40 -0400

    sm8550-common: Use Lineage charger
    Change-Id: I76caaca0616ec2b2e7af9b16b29eaf6ffb00b1a0

--- common.mk
+++ common.mk
@@ -85,9 +85,6 @@ PRODUCT_COPY_FILES += \
 $(call soong_config_set_bool,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
 $(call soong_config_set,qtiaudio,pal_spkr_protection_path,/mnt/vendor/persist/factory/audio/audio.cal)
 
-# Charger
-WITH_LINEAGE_CHARGER := false
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
@@ -132,7 +129,6 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     fstab.qcom.vendor_ramdisk \
     fstab.qcom.zram \
-    init.mmi.charge_only.rc \
     init.mmi.chipset.rc \
     init.mmi.rc \
     init.qcom.rc \
